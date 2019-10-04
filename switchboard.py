98497676363614d77fd9ad5330cbdab9aabec2fc
Mikhail <mschee@physics.utoronto.ca>
2019-10-04 12:04:04 -0400

    Added switchboard file. Contents is nonsense for now

---
+++ switchboard.py
@@ -0,0 +1,12 @@
+# Parameters for running on my local machine
+
+# Stop times for the simulation
+sim_period_stop = 10 # oscillation periods, time in seconds calculated below
+wall_time_stop = 60 # min
+
+# Determine whether adaptive time stepping is on or off
+adapt_dt = False
+
+# Number of points in each dimension
+n_x = 512
+n_z = 512-40
