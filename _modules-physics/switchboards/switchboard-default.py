e24de0465a450fef1f03165f874a6abdaf5b8bee
Mikhail <mschee@physics.utoronto.ca>
2019-10-10 10:13:31 -0400

    Changed simulation domain size

--- _modules-physics/switchboards/switchboard-default.py
+++ _modules-physics/switchboards/switchboard-default.py
@@ -33,16 +33,16 @@ restart_file  = 'restart.h5'
 # Domain parameters
 
 # Dimensions of simulated domain
-L_x = 3.0                   # [m]
-L_z = 1.0                   # [m]
+L_x = 1.5                   # [m]
+L_z = 1.5                   # [m]
 z_t = 0.0
 
 # Dimensions of displayed domain (should be leq simulated domain)
-L_x_dis = 1.0               # [m]
-L_z_dis = 1.0               # [m]
+L_x_dis = 0.5               # [m]
+L_z_dis = 0.5               # [m]
 
 # Display buffer (measured from top left corner)
-Dis_buff_x = 1.0            # [m]
+Dis_buff_x = 0.5            # [m]
 Dis_buff_z = 0.0            # [m]
 
 # Upper left corner of display domain is always (0, 0)
@@ -51,9 +51,6 @@ z_0     = 0.0
 # Therefore, the upper left corner of the simulated domain is
 x_sim_0 = -Dis_buff_x
 z_sim_0 =  Dis_buff_z
-
-# not sure why, but this needs to be here, above imports from bf file
-#lam_x = L_x / 3.0
 
 ###############################################################################
 # Physical parameters
