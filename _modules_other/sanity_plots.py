1ab2ba0089822e0a963d6d75f62c25f4cb13f0a6
Mikhail <mschee@physics.utoronto.ca>
2019-10-25 11:52:49 -0400

    Added boundary forcing window to sanity plots

--- _modules_other/sanity_plots.py
+++ _modules_other/sanity_plots.py
@@ -17,6 +17,7 @@ from docopt import docopt
 # For plotting
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+import matplotlib.gridspec as gridspec
 
 ###############################################################################
 
@@ -60,11 +61,12 @@ dis_ratio = sbp.vp_dis_ratio
 ylims = [z_b, z_t]
 
 # Set up multifigure plot
-rows = 1
+rows = 3
 columns = 3
-fig_len = 9
-fig_wid = columns*0.5*fig_len + 3
+fig_len = 13
+fig_wid = 13#columns*0.3*fig_len - 1
 fig = plt.figure(figsize=(fig_wid, fig_len))
+gridspec.GridSpec(rows, columns)
 ax = []
 # Change the size of the text overall
 font = {'size' : sbp.font_size}
@@ -92,7 +94,8 @@ import background_profile as bp
 build_bp_array = bp.build_bp_array
 bp_array = build_bp_array(z)
 # Plotting
-ax.append( fig.add_subplot(rows,columns,1))
+ax.append( plt.subplot2grid((rows, columns), (0,0), rowspan=2, fig=fig))
+#ax.append( fig.add_subplot(rows,columns,1))
 ax[0] = ps.make_vp_plot(ax[0], bp_array, z, sbp.buffer, sbp.extra_buffer, ylims, sbp.vp_dis_ratio, sbp.abs_div)
 set_vp_labels(ax[0], bp_dict)
 
@@ -106,7 +109,8 @@ import sponge_layer as sl
 build_sl_array = sl.build_sl_array
 sl_array = build_sl_array(z)
 # Plotting
-ax.append( fig.add_subplot(rows,columns,2))
+ax.append( plt.subplot2grid((rows, columns), (0,1), rowspan=2, fig=fig))
+#ax.append( fig.add_subplot(rows,columns,2))
 ax[1] = ps.make_vp_plot(ax[1], sl_array, z, sbp.buffer, sbp.extra_buffer, ylims, sbp.vp_dis_ratio, sbp.abs_div)
 set_vp_labels(ax[1], sl_dict)
 
@@ -120,11 +124,24 @@ import rayleigh_friction as rf
 build_rf_array = rf.build_rf_array
 rf_array = build_rf_array(z)
 # Plotting
-ax.append( fig.add_subplot(rows,columns,3))
+ax.append( plt.subplot2grid((rows, columns), (0,2), rowspan=2, fig=fig))
+#ax.append( fig.add_subplot(rows,columns,3))
 ax[2] = ps.make_vp_plot(ax[2], rf_array, z, sbp.buffer, sbp.extra_buffer, ylims, sbp.vp_dis_ratio, sbp.abs_div)
 set_vp_labels(ax[2], rf_dict)
 
 ###############################################################################
+
+# Need to add the path before every import
+sys.path.insert(0, p_module_dir)
+import boundary_forcing as bf
+build_win_array = bf.build_win_array
+window_array = build_win_array(x)
+# Plotting
+ax.append( plt.subplot2grid((rows, columns), (2,0), colspan=columns, fig=fig))
+ax[3].plot(x, window_array)
+ax[3].set_title('Boundary forcing window')
+ax[3].set_xlabel(r'$x$ (m)')
+ax[3].set_ylabel('Window coefficient')
 
 # Characteristic stratification [rad/s]
 N0     = float(sbp.N_0)
@@ -141,5 +158,5 @@ A      = float(sbp.A)
 # Horizontal wavelength
 lam_x  = sbp.lam_x
 
-#fig.savefig(str(savepath), dpi=dpi)
+fig.suptitle(NAME)
 fig.savefig('outputs/sanity_plots.png', dpi=sbp.dpi)
