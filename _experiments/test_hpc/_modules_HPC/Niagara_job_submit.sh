c9161fce5876a4869234beb2282aa7a085bcd800
Mikhail <mschee@physics.utoronto.ca>
2019-11-04 17:29:58 -0500

    Added test_hpc to run 2019-11-04_17h29_test_hpc on Niagara

--- _experiments/test_hpc/_modules_HPC/Niagara_job_submit.sh
+++ _experiments/test_hpc/_modules_HPC/Niagara_job_submit.sh
@@ -46,7 +46,7 @@ pwd
 ###############################################################################
 # Push specified experiment to git, using -f to override .git/info/exclude
 git add -f _experiments/${NAME}/*
-git commit -m "Added ${NAME} to run ${RUN_NAME} on supercomputer"
+git commit -m "Added ${NAME} to run ${RUN_NAME} on Niagara"
 git push
 
 ###############################################################################
@@ -73,6 +73,7 @@ echo "Copying experiment to scratch directory"
 cp -r ${NHOME}/${DIRECTORY}/${SUBDIRECT}/_experiments/${NAME} ${NSCRATCH}/${DIRECTORY}/${SUBDIRECT}/_experiments/
 cd ${NSCRATCH}/${DIRECTORY}/${SUBDIRECT}/_experiments/${NAME}
 pwd
+git pull
 sbatch --job-name=$JOBNAME $LANCEUR_SCRIPT -n ${NAME} -r ${RUN_NAME} -c ${CORES}
 squeue -u mschee
 EOF
