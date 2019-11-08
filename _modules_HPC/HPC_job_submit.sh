e9d692f54590633ae1ae093d830ccc0fa8fae67f
Mikhail <mschee@physics.utoronto.ca>
2019-11-08 10:36:07 -0500

    Added -s opt, using scp from local to transfer exp dir

--- _modules_HPC/HPC_job_submit.sh
+++ _modules_HPC/HPC_job_submit.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-# A bash script to submit a job to Niagara
+# A bash script to submit a job to HPC
 # To be run from experiment directory after called by run.sh
 # Takes in arguments:
-#	$ bash Niagara_job_submit.sh -n <exp_name>
-#								 -r <run name>
-#								 -h <HPC resource: Niagara, Graham, etc.>
-#								 -c <cores>
+#	$ bash HPC_job_submit.sh -n <exp_name>
+#							 -r <run name>
+#							 -h <HPC resource: Niagara, Graham, etc.>
+#							 -c <cores>
+#							 -s <sanity: pause to check setup before running>
 
 # Current datetime
 DATETIME=`date +"%Y-%m-%d_%Hh%M"`
 
-while getopts n:r:h:c: option
+while getopts n:r:h:c:s: option
 do
 	case "${option}"
 		in
@@ -18,6 +19,7 @@ do
 		r) RUN_NAME=${OPTARG};;
 		h) HPC=${OPTARG};;
 		c) CORES=${OPTARG};;
+		s) SANITY=${OPTARG};;
 	esac
 done
 
@@ -42,62 +44,84 @@ then
     CORES=32
 	echo "No number of cores specified, using CORES=${CORES}"
 fi
+if [ -z "$SANITY" ]
+then
+    SANITY=0
+	echo "No saity specified, using SANITY=${SANITY}"
+fi
 LOC=0
 
 ###############################################################################
-# Navigate to project directory
-cd ..
-cd ..
-pwd
-
-###############################################################################
-# Push specified experiment to git, using -f to override .git/info/exclude
-git add -f _experiments/${NAME}/*
-git commit -m "Added ${NAME} to run ${RUN_NAME} on HPC"
-git push
-
-###############################################################################
-# Prepare scratch
+# Prepare variables
 DATE=`date +"%m-%d_%Hh%M"`
 JOBNAME=$RUN_NAME
 DIRECTORY='Dedalus_Projects'
 SUBDIRECT='template_dedalus_repository'
 SSH_KEY='~/.ssh/id_rsa'
+LANCEUR_SCRIPT="_modules_HPC/${HPC}_lanceur.slrm"
+USER='mschee'
+LOCAL='ngws19.atmosp.physics.utoronto.ca'
+LOCAL_DIR="/home/${USER}/Documents/Dedalus_Projects/example_projects"
 
 if [ $HPC = 'Niagara' ]
 then
-	NHOME='/home/n/ngrisoua/mschee'
-	NSCRATCH='/scratch/n/ngrisoua/mschee'
-	LANCEUR_SCRIPT='_modules_HPC/Niagara_lanceur.slrm'
-	SSH_LOGIN='mschee@niagara.scinet.utoronto.ca'
+	NHOME="/home/n/ngrisoua/${USER}"
+	NSCRATCH="/scratch/n/ngrisoua/${USER}"
+	SSH_LOGIN="${USER}@niagara.scinet.utoronto.ca"
 elif [ $HPC = 'Graham' ]
 then
-	NHOME='/home/mschee'
-	NSCRATCH='/scratch/mschee'
-	LANCEUR_SCRIPT='_modules_HPC/Graham_lanceur.slrm'
-	SSH_LOGIN='mschee@graham.computecanada.ca'
+	NHOME="/home/${USER}"
+	NSCRATCH="/scratch/${USER}"
+	SSH_LOGIN="${USER}@graham.computecanada.ca"
 elif [ $HPC = 'Cedar' ]
 then
-	NHOME='/home/mschee'
-	NSCRATCH='/scratch/mschee'
-	LANCEUR_SCRIPT='_modules_HPC/Cedar_lanceur.slrm'
-	SSH_LOGIN='mschee@cedar.computecanada.ca'
+	NHOME="/home/${USER}"
+	NSCRATCH="/scratch/${USER}"
+	SSH_LOGIN="${USER}@cedar.computecanada.ca"
 fi
+
+###############################################################################
+# Secure copy experiment folder from local to HPC
 
 echo ''
 echo "--Logging in to ${HPC}--"
 # Log in to HPC, execute commands until EOF, then exit
 #	The -i flag points to an rsa file so I shouldn't need to enter my password
 ssh -i $SSH_KEY $SSH_LOGIN << EOF
+echo "We're in"
+cd ${NSCRATCH}/${DIRECTORY}/${SUBDIRECT}/_experiments
+if [ -e ${NAME} ]
+then
+    echo "An old version of ${NAME} will be removed."
+	if [ $SANITY -eq 1 ]
+	then
+		read -p 'Press enter to continue or Ctrl+c to cancel.'
+	fi
+	rm -rf ${NAME}
+fi
+mkdir ${NAME}
 echo ''
-cd ${DIRECTORY}/${SUBDIRECT}
-echo "Pulling from git:"
-git pull
+EOF
+
+# A bit of a round-about way to make sure I never try to transfer the snapshots folder
+echo "Copying ${NAME} from local to ${HPC} scratch directory"
+cd ${LOCAL_DIR}/${SUBDIRECT}/_experiments/${NAME}
+for file in ./*
+do
+	if [ ${file} != './snapshots' ]
+	then
+		#echo $file
+		scp -r $file ${SSH_LOGIN}:${NSCRATCH}/${DIRECTORY}/${SUBDIRECT}/_experiments/${NAME}
+	fi
+done
+
 echo ''
-echo "Copying experiment to scratch directory"
-cp -r ${NHOME}/${DIRECTORY}/${SUBDIRECT}/_experiments/${NAME} ${NSCRATCH}/${DIRECTORY}/${SUBDIRECT}/_experiments/
+echo "--Logging in to ${HPC}--"
+# Log in to HPC, execute commands until EOF, then exit
+#	The -i flag points to an rsa file so I shouldn't need to enter my password
+ssh -i $SSH_KEY $SSH_LOGIN << EOF
+echo "We're in"
 cd ${NSCRATCH}/${DIRECTORY}/${SUBDIRECT}/_experiments/${NAME}
-pwd
 sbatch --job-name=$JOBNAME $LANCEUR_SCRIPT -n ${NAME} -r ${RUN_NAME} -c ${CORES}
 squeue -u mschee
 EOF
